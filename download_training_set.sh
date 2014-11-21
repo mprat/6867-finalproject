eff386bd578ecc5a030ad8eeb10cf705a75ad515
Michele Pratusevich <mprat@mit.edu>
2014-11-21 13:54:24 -0500

    actually add the download script

---
+++ download_training_set.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while read line
+do 
+	echo "$line";
+	baseurl="http://labelme.csail.mit.edu/Images/users/antonio/static_sun_database";
+	subdir=`expr "$line" : '\(/[a-z]/[a-z].*/\)'`;
+	# subdir=${subdir:1};
+	# echo "$baseurl""$line";
+	wget -P images"$subdir" "$baseurl""$line";
+	# echo images"$subdir";
+done < Partitions/Training_01.txt
